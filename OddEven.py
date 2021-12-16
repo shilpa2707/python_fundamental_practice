6111b84f474ec637c5065bff9d3227948f927e51
shilpa27tiwari <shilpa_1312@yahoo.in>
2021-12-15 19:15:43 -0600

    new average code added

---
+++ OddEven.py
@@ -0,0 +1,5 @@
+a = int(input ("Enter a number : "))
+if(a % 2 ==0):
+    print(f"Given number is even : {a}")
+else:
+    print(f"Given number is odd : {a}")
---
+++ PositiveNegative.py
@@ -0,0 +1,7 @@
+a  = int(input("Enter a number : "))
+if(a > 0):
+    print(f"Number {a} is positive.")
+elif(a==0):
+    print(f"Number {a} is zero.")
+else:
+    print(f"Number {a} is negative.")
